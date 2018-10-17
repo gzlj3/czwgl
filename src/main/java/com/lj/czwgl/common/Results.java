d2620a7a8cc4c4cecb704194c09ef5210f586887
gzlj3 <gzlj3@163.com>
2018-10-17 22:20:16 +0800

    home

--- src/main/java/com/lj/czwgl/common/Results.java
+++ src/main/java/com/lj/czwgl/common/Results.java
@@ -1,34 +1,44 @@
 package com.lj.czwgl.common;
 
-public class Results<T> {
-	int status=0;
-	String msg="";
-	T data[];
+import java.util.List;
 
-	public Results(int status, String msg, T[] data) {
+public class Results<T> {
+	int status = Constants.REMOTE_SUCCESS;
+	String msg = "";
+	List<T> data = null;
+
+	public Results() {
+	}
+
+	public Results(int status, String msg, List<T> data) {
 		super();
 		this.status = status;
 		this.msg = msg;
 		this.data = data;
 	}
-	
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public String getMsg() {
 		return msg;
 	}
+
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
-	public T[] getData() {
+
+	public List<T> getData() {
 		return data;
 	}
-	public void setData(T[] data) {
+
+	public void setData(List<T> data) {
 		this.data = data;
 	}
-	
+
 }
--- src/main/java/com/lj/czwgl/controller/CzwglController.java
+++ src/main/java/com/lj/czwgl/controller/CzwglController.java
@@ -1,8 +1,11 @@
 package com.lj.czwgl.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,32 +13,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lj.czwgl.common.Results;
 import com.lj.czwgl.domain.House;
-import com.lj.czwgl.repository.HouseRepository;
+import com.lj.czwgl.service.ICzwglService;
+import com.lj.czwgl.utils.Utils;
 
 @Controller
 @ResponseBody
 @RequestMapping(path = "/fygl")
 public class CzwglController {
 
+	private final String yyhid = "1";
+
 	@Autowired
-	private HouseRepository houseRepository;
+	private ICzwglService czwglService;
 
 	@GetMapping(path = "/fygl_list")
-	public Iterable<House> getFyglList() {
-		Iterable<House> result = houseRepository.findAll();
-		return result;
+	public Results<House> getFyglList() {
+		Results<House> results = new Results<House>();
+		try {
+			List<House> result = czwglService.getFyglList();
+			results.setData(result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return results;
 	}
 
-	@PostMapping(path = "/fygl_list")
-	public Results<House> postFyglList(@RequestBody String s) {
-		return new Results<House>(0, "", null);
+	@PostMapping(path = "/fygl_list/{action}")
+	public Results<House> postFyglList(@PathVariable short action,
+			@RequestBody House house) {
+		try {
+			String id = Utils.getUUID32();
+			house.setHouseid(id);
+			house.setYyhid(yyhid);
+			czwglService.addFy(house);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new Results<House>();
 	}
 
-	@GetMapping(path = "/add")
-	public String addNewUser() {
-		House n = new House();
-		n.setHouseid("1");
-		houseRepository.save(n);
-		return "Saved";
-	}
+	// @GetMapping(path = "/add")
+	// public String addNewUser() {
+	// House n = new House();
+	// n.setHouseid("1");
+	// houseRepository.save(n);
+	// return "Saved";
+	// }
 }
