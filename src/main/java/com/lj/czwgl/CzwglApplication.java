1e13e72c678724c13418cbec38de0a6372e47ad8
gzlj3 <gzlj3@163.com>
2018-10-17 10:29:13 +0800

    init

---
+++ src/main/java/com/lj/czwgl/CzwglApplication.java
@@ -0,0 +1,12 @@
+package com.lj.czwgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzwglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzwglApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lj/czwgl/CzwglApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lj.czwgl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CzwglApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
